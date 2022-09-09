1de623c0957c08ef83149aa9c5567c1175b81923
Bridget <bridgetrosefitz@gmail.com>
2022-09-09 14:40:02 +0100

    exercise 2 - allows custom serialiser / deserialiser for the thing being stored in local storage

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,16 +4,27 @@
 import React, { useEffect, useState } from 'react'
 
 
-const useLocalStorageState = (key) => {
-  // get item from local storage
-  const initialItem = () => localStorage.getItem(key)
+const useLocalStorageState = (key, serialiser=JSON.stringify, deserialiser=JSON.parse) => {
+
+const initialItemParsed = () => {
+
+    // get item from local storage
+    const initialItem = localStorage.getItem(key)
+    
+    // parse the string into its underlying data type
+
+    return initialItem ? deserialiser(initialItem) : undefined // This is the same behaviour you'd expect from useState if you don't pass it an initial value
+  }
 
   // store item from local storage in state
-  const [item, setItem] = useState(initialItem)
+  const [item, setItem] = useState(initialItemParsed)
 
   // update local storage when there's a state change
   useEffect(() => {
-    localStorage.setItem(key, item)
+
+    // serialise / stringify the data first
+    const serialisedItem = serialiser(item)
+    localStorage.setItem(key, serialisedItem)
   }, [item, key])
 
   return [item, setItem]
